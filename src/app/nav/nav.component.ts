24709bb04c1cda58e4aaf574edd3166203acd59c
Vijay Deepak <vijaydeepak7195@outlook.com>
2018-10-24 12:02:46 +0530

    initial component setup

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+// tslint:disable-next-line:no-inferrable-types
+  heading: string = 'A7 Site';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
